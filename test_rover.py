32699ed5d85b3a1982fac69d116edf3402914fc8
k2controls <kkelly@k2controls.net>
2021-11-18 03:24:58 -0500

    Initial commit

---
+++ test_rover.py
@@ -0,0 +1,12 @@
+''' Runs a series of test on Rover model
+    asking user to verify behaviors
+'''
+from Rover.rover_factory import make_rover
+from tests.test_rover import test_rover
+
+print("This is a test of the rover unit. Components will be activated")
+print("and you will be asked to verify by entering either Y or N.")
+input("Press Enter to continue...")
+rover = make_rover()
+test_rover(rover)
+print("Test complete.")
